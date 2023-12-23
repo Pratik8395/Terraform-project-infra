3f7aaed0ca111fdbff301f0dc84eb6c740daba66
pratik <pratik.sawale8395@gmail.com>
2023-12-23 11:30:30 +0000

    made changes in output.tf

--- output.tf
+++ output.tf
@@ -20,20 +20,20 @@ output "qa_public_ip" {
 
 # qa private ip
 output "qa_private_ip" {
-        value   =       aws_instance.dev_test_1.private_ip
+        value   =       aws_instance.qa_test_1.private_ip
 }
 
 
 
 # uat public ip
 output "uat_public_ip" {
-        value   =       aws_instance.dev_test_1.public_ip
+        value   =       aws_instance.uat_test_1.public_ip
 }
 
 
 
 # uat private ip
 output "uat_private_ip" {
-        value   =       aws_instance.dev_test_1.private_ip
+        value   =       aws_instance.uat_test_1.private_ip
 }
 
